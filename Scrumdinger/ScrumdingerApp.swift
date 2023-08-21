abc5c420f10a8014da1433c5d25d9c42145c6b11
Brennan Rivera <brennanrivera0@gmail.com>
2023-08-20 21:26:23 -0400

    * Added timer feature * Imported assets folder * Working on formatting user data on UI

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Brennan Rivera on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
